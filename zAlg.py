7ccbca027afa6190309c80a2d430f29c1925b8c8
Trevor Gahl <tgahl7#gmail.com>
2017-09-09 12:11:50 -0600

    Added exact match

--- zAlg.py
+++ zAlg.py
@@ -2,7 +2,7 @@ def zAlg(s):
 
     # This builds the z string
     Z = [0] * len(s)
-    Z[0] = len(s)
+    Z[0] = 0
 
     # initialize the right and left pointers
     rt = 0
@@ -35,6 +35,35 @@ def zAlg(s):
     return Z
 
 
-t = ['x', 'a', 'a', 'a', 'x', 'a', 'a', 'y']
+def exactMatch(p, t):
+    patternLength = len(p)
+    print(patternLength)
+    sentinel = ['$']
+    maxMatch = 0
+    match = 0
+    test = p + sentinel + t
+    print(test)
+    zOut = zAlg(test)
+    print(zOut)
+    # print(len(zOut))
+    for i in range(len(zOut)):
+        # print(patternLength - 1)
+        if i < patternLength:
+            print("test")
+        else:
+            print(zOut[i])
+            if zOut[i] == patternLength:
+                print("bem")
+                match += 1
+            else:
+                if zOut[i] > maxMatch:
+                    maxMatch += zOut[i]
+    return match, maxMatch
+
+
+t = ['x', 'a', 'a', 'a', 'x', 'a', 'b', 'y']
 testRun = zAlg(t)
 print(testRun)
+t2 = ['x', 'a', 'a']
+
+print(exactMatch(t2, t))
