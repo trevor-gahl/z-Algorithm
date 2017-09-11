46fcb2b311dbb9ff897078f5f866581fc0ed9133
David Schwehr <dpschwehr@gmail.com>
2017-09-11 13:41:41 -0600

    added user input for string and patter

--- zAlg.py
+++ zAlg.py
@@ -1,6 +1,21 @@
+#!/bin/bash/python
+'''
+*************************************************
+***  CSCI-451 Fall 2017 Z-algorithm           ***
+***  Treavor Gahl, David Schwehr, Cas Loftin  ***
+***                                           ***
+***  Utilizing Z-algorithm to search for a    ***
+***  pattern in a string in linear time.      ***
+***  Gets user input for string and pattern,  ***
+***  returns number of matches and length of  ***
+***  longest match.                           ***
+************************************************* 
+'''
+
+
 def zAlg(s):
 
-    # This builds the z string
+    # build an array of z-values
     Z = [0] * len(s)
     Z[0] = 0
 
@@ -37,33 +52,51 @@ def zAlg(s):
 
 def exactMatch(p, t):
     patternLength = len(p)
-    print(patternLength)
+    #print(patternLength)
     sentinel = ['$']
     maxMatch = 0
     match = 0
     test = p + sentinel + t
-    print(test)
+    #print(test)
     zOut = zAlg(test)
-    print(zOut)
+    #print(zOut)
     # print(len(zOut))
     for i in range(len(zOut)):
         # print(patternLength - 1)
         if i < patternLength:
             print("test")
         else:
-            print(zOut[i])
+            #print(zOut[i])
             if zOut[i] == patternLength:
-                print("bem")
+                #print("bem")
                 match += 1
             else:
                 if zOut[i] > maxMatch:
                     maxMatch += zOut[i]
     return match, maxMatch
 
+def main():
+    sequence = input("Enter string, S, no spaces: \n") 
+    pattern = input("Enter pattern, P, that you want to find mathces in string: \n")
+    sequenceList = list(sequence)
+    patternList = list(pattern)
+    print(sequenceList)
+    print(patternList)
+    zValues = zAlg(patternList)
+    print("Z-values: " + str(zValues))
+    #print(zValues)
+    matches = exactMatch(patternList, sequenceList)
+    print(matches)
+    print("Number of matches: " + str(matches[0]))
+    print("Longest match: " + str(matches[1]))
 
+'''
 t = ['x', 'a', 'a', 'a', 'x', 'a', 'b', 'y']
 testRun = zAlg(t)
 print(testRun)
 t2 = ['x', 'a', 'a']
 
 print(exactMatch(t2, t))
+'''
+if __name__ == "__main__":
+    main()
