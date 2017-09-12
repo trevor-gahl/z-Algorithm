e155b9dab1cf5dbcf607355cb8314855579078cb
Trevor Gahl <tgahl7#gmail.com>
2017-09-12 13:50:26 -0600

    Added advanced algorithm

--- zAlg.py
+++ zAlg.py
@@ -57,14 +57,14 @@ def exactMatch(p, t):
     maxMatch = 0
     match = 0
     test = p + sentinel + t
-    # print(test)
-    zOut = zAlg(test)
-    # print(zOut)
+    print(test)
+    zOut = z_advanced(test)
+    print(zOut)
     # print(len(zOut))
     for i in range(len(zOut)):
         # print(patternLength - 1)
         if i < patternLength:
-            print("test")
+            continue
         else:
             # print(zOut[i])
             if zOut[i] == patternLength:
@@ -78,15 +78,53 @@ def exactMatch(p, t):
     return match, maxMatch
 
 
+def z_advanced(s):
+    """An advanced computation of Z-values of a string."""
+
+    Z = [0] * len(s)
+    Z[0] = len(s)
+
+    rt = 0
+    lt = 0
+
+    for k in range(1, len(s)):
+        if k > rt:
+            # If k is outside the current Z-box, do naive computation.
+            n = 0
+            while n + k < len(s) and s[n] == s[n + k]:
+                n += 1
+            Z[k] = n
+            if n > 0:
+                lt = k
+                rt = k + n - 1
+        else:
+            # If k is inside the current Z-box, consider two cases.
+
+            p = k - lt  # Pair index.
+            right_part_len = rt - k + 1
+
+            if Z[p] < right_part_len:
+                Z[k] = Z[p]
+            else:
+                i = rt + 1
+                while i < len(s) and s[i] == s[i - k]:
+                    i += 1
+                Z[k] = i - k
+
+                lt = k
+                rt = i - 1
+    return Z
+
+
 def main():
     sequence = input("Enter string, S, no spaces: \n")
     pattern = input(
-        "Enter pattern, P, that you want to find mathces in string: \n")
+        "Enter pattern, P, that you want to find matches in string: \n")
     sequenceList = list(sequence)
     patternList = list(pattern)
     print(sequenceList)
     print(patternList)
-    zValues = zAlg(sequenceList)
+    zValues = z_advanced(sequenceList)
     print("Z-values: " + str(zValues))
     # print(zValues)
     matches = exactMatch(patternList, sequenceList)
